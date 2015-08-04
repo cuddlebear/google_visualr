f49610e75f07bbddb78ef2f88b37a5440be7297d
Cuddlebear <jan.de-bugger@web.de>
2015-08-04 14:40:50 +0200

    changed include to calendar

--- lib/google_visualr/interactive/calendar.rb
+++ lib/google_visualr/interactive/calendar.rb
@@ -3,7 +3,7 @@ module GoogleVisualr
 
     # http://code.google.com/apis/chart/interactive/docs/gallery/barchart.html
     class Calendar < BaseChart
-      include GoogleVisualr::Packages::CoreChart
+      include GoogleVisualr::Packages::Calendar
 
       # For Configuration Options, please refer to:
       # http://code.google.com/apis/chart/interactive/docs/gallery/barchart.html#Configuration_Options
